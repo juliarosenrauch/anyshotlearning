d9d6debbcd78aabb5d213a478b2c49a2f6e31ccd
Julia Rosenrauch <jul_rose@yahoo.ca>
2022-01-04 17:15:27 -0800

    commit for test deployment

--- demo/app.py
+++ demo/app.py
@@ -6,8 +6,9 @@ import markdowns
 
 import pandas as pd
 import numpy as np
+from PIL import Image
 
-ROOT_DIR = "images"
+ROOT_DIR = "/images"
 
 img_data = [
     join(ROOT_DIR, "select_images/bus.jpg"),
@@ -21,32 +22,32 @@ img_data = [
 imgs = [open(i, 'rb').read() for i in img_data]
 
 loaded_images = {
-    "Bus": imgs[0],
-    "Couch": imgs[1],
-    "Fruits": imgs[2],
-    "Sheep": imgs[3],
-    "Tennis": imgs[4],
-    "Umbrella": imgs[5],
+    "A": imgs[0],
+    "B": imgs[1],
+    "C": imgs[2],
+    "D": imgs[3],
+    "E": imgs[4],
+    "F": imgs[5],
 }
 
 show_rec = False
 
 def main():
     st.set_page_config(page_title="2163 - Any Shot Learning",
-                        page_icon="images/cv_lab.png",
-                        layout="wide",
-                        initial_sidebar_state='expanded')
+                        page_icon="images/cvlab.png",
+                        layout="wide")
+                        # initial_sidebar_state='expanded'
 
-    st.sidebar.header('2163 - Any Shot Learning')
-    selected_demo = st.sidebar.selectbox('Select a Demo', np.array(["test1", "test2"]))
+    # st.sidebar.header('2163 - Any Shot Learning')
+    # selected_demo = st.sidebar.selectbox('Select a Demo', np.array(["test1", "test2"]))
     st.title('In progress UniT demo')
     
     # background on image level vs instance level annotation
     # wouldn't it be nice to be able to transfer knowledge from base classes to novel!
     st.markdown(markdowns.motivation_title, unsafe_allow_html=True)
-    with st.beta_expander("See more"):
+    with st.expander("See more"):
         st.markdown(markdowns.motivation_string, unsafe_allow_html=True)
-        bcol1, bcol2, bcol3 = st.beta_columns([1, 1, 1])
+        bcol1, bcol2, bcol3 = st.columns([1, 1, 1])
         bcol2.image("images/image_vs_instance.png", width=550)
 
     # intro to UBC computer vision lab
@@ -54,31 +55,34 @@ def main():
 
     # theory on UniT paper
     st.markdown(markdowns.unit_title, unsafe_allow_html=True)
-    with st.beta_expander("See more"):
+    with st.expander("See more"):
         st.markdown(markdowns.unit_string, unsafe_allow_html=True)
-        bcol1, bcol2, bcol3 = st.beta_columns([1, 1, 1])
+        bcol1, bcol2, bcol3 = st.columns([1, 1, 1])
         bcol2.image("images/unit.png", width=550)
 
     # about the dataset(s)
     # what kind of annotations there are 
-    st.markdown(markdowns.dataset_title, unsafe_allow_html=True)
-    with st.beta_expander("See more"):
-        st.markdown(markdowns.dataset, unsafe_allow_html=True)
+    # st.markdown(markdowns.dataset_title, unsafe_allow_html=True)
+    # with st.expander("See more"):
+    #     st.markdown(markdowns.dataset, unsafe_allow_html=True)
 
     # dataset in context of paper - base vs novel? which classes?
 
 
     # select image of provided images
     st.markdown(markdowns.object_selection_header, unsafe_allow_html=True)
-    radio_selection_col, image_selection_col, col3 = st.beta_columns([2, 1, 1])
+    radio_selection_col, image_selection_col, col3 = st.columns([2, 1, 1])
+    col1, col2, col3, col4, col5, col6 = st.columns([1,1,1,1,1,1])
     with radio_selection_col:
         st.write(markdowns.radio_selection_styles, unsafe_allow_html=True)
         selected_image = st.radio("", list(loaded_images.keys()))
 
     with image_selection_col:
         st.image(loaded_images[selected_image],
-                 caption='Selected Object',
-                 width=350)
+                caption='Selected Object',
+                width=350)
+
+
     # OR
     # upload your own
     st.markdown(markdowns.upload_file_header, unsafe_allow_html=True)
@@ -99,7 +103,7 @@ def main():
         # background on model
 
     # RUN!
-    col1, col2, col3 = st.beta_columns([1, 1, 1])
+    col1, col2, col3 = st.columns([1, 1, 1])
     st.markdown(markdowns.run_model_button, unsafe_allow_html=True)
     global show_rec
     if col2.button("Run Model"):
@@ -109,14 +113,25 @@ def main():
     if show_rec:
         st.write("reconstruction")
 
-    threshold = st.slider("Threshold Value",
-                          min_value=-0.3,
-                          max_value=0.3,
-                          value=0.0,
-                          step=0.001)
-    if threshold:
-        st.write("show threshold diff")
+    # threshold = st.slider("Threshold Value",
+    #                       min_value=-0.3,
+    #                       max_value=0.3,
+    #                       value=0.0,
+    #                       step=0.001)
+    # if threshold:
+    #     st.write("show threshold diff")
 
+    st.download_button(
+        label = 'Download some text', 
+        data = '''text_contents''', 
+        file_name = "my_file.xml", 
+        mime='application/xml')
+
+    # with open(uploaded_file, "rb'") as file:
+    #     btn = st.download_button(label="Download data as CSV", 
+    #                     data=uploaded_file, 
+    #                     file_name='uploaded_file.png', 
+    #                     mime='image/png')
 
 if __name__ == "__main__":
     main()
